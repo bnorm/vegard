f8e38037e65308acf30d51f87dfe3b834027d29e
Brian Norman <brian@bnorm.com>
2020-07-18 12:16:10 -0500

    Fix JwtServiceTest

--- backend/service/src/test/kotlin/com/bnorm/vegard/auth/JwtServiceTest.kt
+++ backend/service/src/test/kotlin/com/bnorm/vegard/auth/JwtServiceTest.kt
@@ -32,7 +32,7 @@ class JwtServiceTest {
 
   @ParameterizedTest(name = "Known good token can be verified")
   @CsvSource(
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoianVuaXQiLCJleHAiOjkyMjMzNzIwMzY4NTQ3NzV9.H4A5b-6C7aWmQ_RnC4OxwYVGQ-9KMJWTlikWT1zlVIhvaMG2djE2nFqAGpGGlT7YUc80AcEZ8Sbv7FJeOIUGag"
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoianVuaXQiLCJyb2xlIjoidXNlciIsImV4cCI6OTIyMzM3MjAzNjg1NH0.3_aEc_ILrNnwLQgPpIHe8TfUy3xqAEOumOy6JwJYAf7s-maEEl65W2N4XXtAUz-B9ardxQE8kBlzNHr3u3X6og"
   )
   fun `known good tokens can be verified`(token: String) {
     assertDoesNotThrow {
@@ -42,11 +42,11 @@ class JwtServiceTest {
 
   @ParameterizedTest(name = "Known bad token fails verification because: {1}")
   @CsvSource(
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoianVuaXQiLCJleHAiOjB9.GFs2_79z1YDB0kY8mC_tmFTdEP9ySAu3it9Xq-PfBfWQQVllBikhqsgSOYjnPlxESUX52w5hlehd5dMS5LVvHg," +
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoianVuaXQiLCJyb2xlIjoidXNlciIsImV4cCI6MH0.sH3h_DLoqArJsScit9pW9zTHPHq1L7kgObH_t7uZBUw85QH4NJgprj55ac2rEBHW6O5Ja6fkGpL79Y041HuV0A," +
       "The Token has expired on Thu Jan 01 00:00:00 UTC 1970.",
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoibm90LWp1bml0IiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc2fQ.a8eN_82Aj67bOV1z_FbSJE3npns5vkoXeY2ieVRVV73efNQbgTPcdLnNOEZYHaIZBv-C49HdTP3BSmYDEwg0OQ," +
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoibm90LWp1bml0Iiwicm9sZSI6InVzZXIiLCJleHAiOjkyMjMzNzIwMzY4NTR9.Z8qKwAC95rzS2CxUt3pJ7fsQae6RTeBqk3BXHPictFOTtFGaKptdKosPZWhIBnhCdZFHDX1rHIuyL0aJypBxRA," +
       "The Claim 'iss' value doesn't match the required issuer.",
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoianVuaXQiLCJleHAiOjkyMjMzNzIwMzY4NTQ3NzZ9.vair_m3yFw6gNPL7ULH1PXgdwLv0Oo3QZ-yMU61PMyzbjdFX-zUhYW6tknp1LKgO08kJJft7Xu0tNcNvPsW6bA," +
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoianVuaXQiLCJyb2xlIjoidXNlciIsImV4cCI6OTIyMzM3MjAzNjg1NH0.AVeu225uDFcDdkI2vWvYI5JkVKyWbuppy3I4iipd32npbqnP-ns-KUWkYDpOsungR9I0x2B1i7PN99Lr3gnw5g," +
       "The Token's Signature resulted invalid when verified using the Algorithm: HmacSHA512"
   )
   fun `known bad tokens fail verification`(token: String, message: String) {
